7ab72cebfbd6232147fad6d5cf12f304c53e9f09
coraxyc <ycxing99@gmail.com>
2019-04-06 16:44:48 -0700

    Add css and counter-component js

---
+++ js/counter-component.js
@@ -0,0 +1,33 @@
+function printCounterTable() {
+    // Create table string to print
+    var table = "";
+
+    var header = getHeader();
+    var antidoteLine = getAntidoteLine();
+    var poisonLine = getPoisonLine();
+    var riddleLine = getRiddleLine();
+
+    table = header + antidoteLine + poisonLine + riddleLine;
+
+    // Print the table
+}
+
+function getHeader() {
+    var header =    "----------------\n" +
+                    "| Remaining    |\n" +
+                    "----------------\n";
+
+
+}
+
+function getAntidoteLine() {
+
+}
+
+function getPoisonLine() {
+
+}
+
+function getRiddleLine() {
+
+}
