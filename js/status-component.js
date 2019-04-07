da841833a15631d3c679485edbd34504e5c3a49e
coraxyc <ycxing99@gmail.com>
2019-04-06 21:42:08 -0700

    Modify answer hover

--- js/status-component.js
+++ js/status-component.js
@@ -35,20 +35,42 @@ function generateRiddle() {
     switch(index) {
         case 0:
             riddleStr = "Of males and females that attend hackathons, what's the average number each gender attends? <br><br>";
-            riddleStr += "A. Males: 3, Females: 3 <br>";
-            riddleStr += "B. Males: 4.28, Females: 2.36 <br>";
-            riddleStr += "C. Males: 3.28, Females: 4.36 <br>";
+            riddleStr += "<span id='a'class='answer'>A. Males: 3, Females: 3 <br></span>";
+            riddleStr += "<span id='b' class='answer'>B. Males: 4.28, Females: 2.36 <br></span>";
+            riddleStr += "<span id='c' class='answer'>C. Males: 3.28, Females: 4.36 <br></span>";
             break;
         case 1:
+            riddleStr = "Who was the first female Nobel prize winner? <br><br>";
+            riddleStr += "A. Rosalind Franklin <br>";
+            riddleStr += "B. Marie Curie <br>";
+            riddleStr += "C. Grace Hopper <br>";
             break;
         case 2:
+            riddleStr = "Who invented the first compiler? <br><br>";
+            riddleStr += "A. Grace Hopper <br>";
+            riddleStr += "B. Ada Lovelace <br>";
+            riddleStr += "C. Katherine Johnson <br>";
             break;
         case 3:
+            riddleStr = "What did Rosalind Franklin help shape our knowledge about? <br><br>";
+            riddleStr += "A. The sewing machine<br>";
+            riddleStr += "B. DNA model<br>";
+            riddleStr += "C. Dinosaurs<br>";
             break;
         case 4:
+            riddleStr = "Who wrote the first algorithm meant to be processed by a machine? <br><br>";
+            riddleStr += "A. Charles Babbage<br>";
+            riddleStr += "B. Alan Turing<br>";
+            riddleStr += "C. Ada Lovelace<br>";
+            break;
+        case 5:
+            riddleStr = "What did Rosalind Franklin help shape our knowledge about? <br><br>";
+            riddleStr += "A. The sewing machine<br>";
+            riddleStr += "B. DNA model<br>";
+            riddleStr += "C. Dinosaurs<br>";
             break;
         default:
-            return "Unknown index";
+            riddleStr = "Unknown index";
             break;
     }
 
