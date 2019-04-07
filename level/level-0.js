b0f5d5d8e76fe48101e3ba421a94494d731e1f40
annsudhart <sudharta.andrea@gmail.com>
2019-04-07 12:12:00 -0700

    Sprite shows up!

--- level/level-0.js
+++ level/level-0.js
@@ -18,5 +18,5 @@ function createLevel0() {
         }
     }
     console.log('grid created');
-    $('#map-grid').append('<div id="avatar"></div> ');
+    $('#map-grid').append('<div id="avatar"></div>');
 }
