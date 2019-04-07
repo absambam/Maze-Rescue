425a9e8bacd075e04660df5848f634a2298b582d
annsudhart <sudharta.andrea@gmail.com>
2019-04-06 20:08:41 -0700

    variables

--- js/sprite-movement.js
+++ js/sprite-movement.js
@@ -4,19 +4,78 @@ document.addEventListener('keydown', moveUp);
 
 
 function moveUp(e) {
-    console.log(e.which);
-    if (e.which == 38) {
-        var currVal = parseInt($('#avatar').css('top'), 10)
-        if (currVal >= 28) {
-            var newVal = currVal - 100;
-            $('#avatar').css('top', newVal + 'px');
+    if (unvisitedCount > 0) {
+        if (e.which == 37) {
+            // go left
+            if (x > 0) {
+                if (currGrid[y][x - 1] !== '.') {
+                    var currVal = parseInt($('#avatar').css('left'), 10)
+                    var newVal = currVal - 102;
+                    $('#avatar').css('left', newVal + 'px');
+                    x--;
+                    unvisitedCount--;
+                    if (unvisitedCount == 0) {
+                        $('body').append('<h2>Game Over</h2>') 
+                    }
+                }
+            }
+        } else if (e.which == 38) {
+            // go up
+            if (y > 0) {
+                if (currGrid[y - 1][x] !== '.') {
+                    var currVal = parseInt($('#avatar').css('top'), 10)
+                    var newVal = currVal - 100;
+                    $('#avatar').css('top', newVal + 'px');
+                    y--;
+                    unvisitedCount--;
+                    if (unvisitedCount == 0) {
+                        $('body').append('<h2>Game Over</h2>')
+                        gameOver = true;
+                    }
+                }
+            }
+        } else if (e.which == 39) {
+            // go right
+            if (x < 3) {
+                if (currGrid[y][x + 1] !== '.') {
+                    var currVal = parseInt($('#avatar').css('left'), 10)
+                    var newVal = currVal + 102;
+                    $('#avatar').css('left', newVal + 'px');
+                    x++;
+                    unvisitedCount--;
+                    if (unvisitedCount == 0) {
+                        $('body').append('<h2>Game Over</h2>')
+                    }
+                }
+            }
+        } else if (e.which == 40) {
+            // go down
+            if (y < 2) {
+                if (currGrid[y + 1][x] !== '.') {
+                    var currVal = parseInt($('#avatar').css('top'), 10)
+                    var newVal = currVal + 100;
+                    $('#avatar').css('top', newVal + 'px');
+                    y++;
+                    unvisitedCount--;
+                    if (unvisitedCount == 0) {
+                        $('body').append('<h2>Game Over</h2>')
+                    }
+                }
+            }
         }
-    } else if (e.which == 39) {
-        var currVal = parseInt($('#avatar').css('left'), 10)
-        if (currVal < 330) {
-            var newVal = currVal + 102;
-            $('#avatar').css('left', newVal + 'px');
-        }
+        currGrid[y][x] = '.';
+        console.log(currGrid);
     }
 }
-console.log('avatar can move up');
+
+var x = 0;
+var y = 2;
+var antidoteCount = 1;
+var poisonCount = 1;
+var riddleCount = 1;
+var unvisitedCount = 11;
+var gameOver = false;
+
+currGrid[y][x] = '.';
+console.log(currGrid);
+console.log(map);
--- level/level-0.js
+++ level/level-0.js
@@ -1,15 +1,19 @@
 /* Creating the map for level 0 */
 
-var map = ['x', 'p', 'x', 'x',
-    'r', 'x', 'x', 'x',
-    'x', 'x', 'a', 'x'];
+var map = [ ['x', 'p', 'x', 'x'],
+            ['r', 'x', 'x', 'x'],
+            ['x', 'x', 'a', 'x']];
+
+var currGrid = [['x', 'p', 'x', 'x'],
+                ['r', 'x', 'x', 'x'],
+                ['x', 'x', 'a', 'x']];
 
 function createLevel0() {
     console.log('map made...');
     console.log($('#map-grid'));
     for (i = 0; i < 3; i++) {
         $('#map-grid').append('<div class="map-row" id="map-row-' + i + '"></div>');
-        for (j = 0; j < 4; j++) {
+        for (j = 0; j < map[i].length; j++) {
             $('#map-row-' + i).append('<div class="map-tile"></div>');
         }
     }
