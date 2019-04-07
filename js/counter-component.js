7bdcd7d15e6966bc38d306202625e129ef8c4cb9
coraxyc <ycxing99@gmail.com>
2019-04-06 17:15:17 -0700

    Created text counter

--- js/counter-component.js
+++ js/counter-component.js
@@ -3,31 +3,40 @@ function printCounterTable() {
     var table = "";
 
     var header = getHeader();
-    var antidoteLine = getAntidoteLine();
-    var poisonLine = getPoisonLine();
-    var riddleLine = getRiddleLine();
+    var antidoteLine = getAntidoteLine(3);
+    var poisonLine = getPoisonLine(2);
+    var riddleLine = getRiddleLine(1);
+    var footer = getFooter();
 
-    table = header + antidoteLine + poisonLine + riddleLine;
+    table = header + antidoteLine + poisonLine + riddleLine + footer;
 
     // Print the table
+    document.getElementById("counterTable").innerHTML = table;
 }
 
 function getHeader() {
-    var header =    "----------------\n" +
-                    "| Remaining    |\n" +
-                    "----------------\n";
-
-
+    var header =    "----------------<br>" +
+                    "| Remaining &nbsp&nbsp&nbsp|<br>" +
+                    "----------------<br>";
+    return header;
 }
 
-function getAntidoteLine() {
-
+function getAntidoteLine(antidoteCount) {
+    var antidoteLine =  "| Antidote &nbsp&nbsp" + antidoteCount + " |<br>";
+    return antidoteLine;
 }
 
-function getPoisonLine() {
-
+function getPoisonLine(poisonCount) {
+    var poisonLine =  "| Poison &nbsp&nbsp&nbsp&nbsp" + poisonCount + " |<br>";
+    return poisonLine;
 }
 
-function getRiddleLine() {
+function getRiddleLine(riddleCount) {
+    var riddleLine =  "| Riddle &nbsp&nbsp&nbsp&nbsp" + riddleCount + " |<br>";
+    return riddleLine;
+}
 
+function getFooter() {
+    var footer = "----------------<br>";
+    return footer;
 }
